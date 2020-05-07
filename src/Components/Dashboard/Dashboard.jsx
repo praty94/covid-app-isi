56bec9dbb2a56a1ebe3b3f4223a922ac0005bc4d
Sandip Dust <riddhi.909@gmail.com>
2020-05-07 14:01:46 +0530

    made changes to the sidebar button

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import {pages} from '../../Data/AppElements';
-import Sidebar from '../Sidebar/Sidebar';
-
-
+import React from "react";
+import { pages } from "../../Data/AppElements";
+import Sidebar from "../Sidebar/Sidebar";
+import styles from "./Dashboard.module.css";
 class Dashboard extends React.Component {
-    state = {
-        currentPage: pages[0]
-    }
-    render() {
-        return (            
-            <Sidebar options={pages} selectedPage={this.state.currentPage} optionSelectedHandler={(page)=>this.setCurrentpage(page)}></Sidebar>                        
-        );
-    }
-    setCurrentpage = (page) => {
-        //updating state only if selected page is different
-        if(page !== this.state.currentPage)
-            this.setState({currentPage:page});
-    }
+  state = {
+    currentPage: pages[0]
+  };
+  render() {
+    return (
+      <div>
+        <button className={styles.openbutton}>Open</button>
+        <Sidebar
+          options={pages}
+          selectedPage={this.state.currentPage}
+          optionSelectedHandler={page => this.setCurrentpage(page)}
+        ></Sidebar>
+      </div>
+    );
+  }
+  setCurrentpage = page => {
+    //updating state only if selected page is different
+    if (page !== this.state.currentPage) this.setState({ currentPage: page });
+  };
 }
 
 export default Dashboard;
