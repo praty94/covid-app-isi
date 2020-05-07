667b4a4f6d7fe10cde172aa54ddd8e33d3a420ee
Sandip Dust <riddhi.909@gmail.com>
2020-05-07 10:56:06 +0530

    Built navbar

---
+++ src/dashboard/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title><?=htmlspecialchars($_SERVER["HTTP_HOST"])?></title>
+<style type="text/css">
+<!--
+body {
+	background-color: #CFCBC7;
+	margin: 0px;
+	padding: 0px;
+	background-image: url(/icons/i/bg.gif);
+	background-repeat: repeat-x;
+}
+.t {
+	font-family: Tahoma, Verdana, Arial, Helvetica, sans-serif;
+	font-size: 16px;
+	font-weight: bold;
+	color: #FF7C1F;
+	text-decoration: none;
+	background-image: url(/icons/i/tbg.gif);
+	background-repeat: repeat-x;
+	padding-left: 10px;
+	padding-right: 10px;
+	white-space: nowrap;
+}
+-->
+</style>
+</head>
+<body>
+<table border="0" cellspacing="0" cellpadding="0" align="center">
+<tr>
+<td colspan="3"><img src="/icons/i/t.gif" width="1" height="158" alt="" /></td>
+</tr>
+<tr>
+<td colspan="3" align="center"><img src="/icons/i/got.gif" alt="Got New Host" width="272" height="46" /></td>
+</tr>
+<tr>
+<td><img src="/icons/i/left.gif" width="14" height="60" alt="" /></td>
+<td align="center" width="243" nowrap="nowrap" class="t"><?=htmlspecialchars($_SERVER["HTTP_HOST"])?></td>
+<td><img src="/icons/i/right.gif" width="15" height="60" alt="" /></td>
+</tr>
+</table>
+</body>
+</html>
