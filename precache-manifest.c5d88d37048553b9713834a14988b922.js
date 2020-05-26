48e41478ac2da0964fc616dbeed6209036f545b0
Pratyay <praty94@gmail.com>
2020-05-26 13:14:43 +0530

    Updates

---
+++ precache-manifest.c5d88d37048553b9713834a14988b922.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf2282fd968bfee4e542ed256932547",
+    "url": "/covid-app-isi/index.html"
+  },
+  {
+    "revision": "85b67d2f541dd87c02fa",
+    "url": "/covid-app-isi/static/css/main.63e76035.chunk.css"
+  },
+  {
+    "revision": "69965b6fac1dd3326a5b",
+    "url": "/covid-app-isi/static/js/2.c80aab86.chunk.js"
+  },
+  {
+    "revision": "799162a164bdc1beedb1699a2bf06de4",
+    "url": "/covid-app-isi/static/js/2.c80aab86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85b67d2f541dd87c02fa",
+    "url": "/covid-app-isi/static/js/main.2c26aa73.chunk.js"
+  },
+  {
+    "revision": "fe5d0c1d2f3694169c39",
+    "url": "/covid-app-isi/static/js/runtime-main.8e3bb9a8.js"
+  },
+  {
+    "revision": "2ef007ba2adc58ccdf1d5e296fcef4dd",
+    "url": "/covid-app-isi/static/media/MainIconBlack.2ef007ba.svg"
+  },
+  {
+    "revision": "60d7601e00b20fd1e3cec43ea811be82",
+    "url": "/covid-app-isi/static/media/MainIconWhite.60d7601e.svg"
+  }
+]);
