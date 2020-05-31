1baced369a548d9e88e32b589d52eaafffa97849
Pratyay <praty94@gmail.com>
2020-05-31 16:44:05 +0530

    changed concentration page name to spread

--- src/Data/AppElements.js
+++ src/Data/AppElements.js
@@ -3,7 +3,7 @@ export const sidebarOptions = [
     {id:2,name:"Weekly Increase",icon:"increase"},
     {id:3,name:"Recovery Rate",icon:"recovery"},
     {id:4,name:"Traffic Intensity",icon:"intensity"},
-    {id:5,name:"Concentration",icon:"concentration"},
+    {id:5,name:"Spread",icon:"concentration"},
     {id:6,name:"Tested Positive",icon:"positive"},
     {id:7,name:"Death Rate",icon:"death"},
     {id:8,name:"FAQ",icon:"faq"},
